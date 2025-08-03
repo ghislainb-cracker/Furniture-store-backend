01271ba1558ab0fed2a6845444f3e2df69d039a5
Ghislain Byimbo <byimbog250@gmail.com>
2025-08-03 14:49:36 +0200

    resolve merge conflict from bimzed

--- models/Product.js
+++ models/Product.js
@@ -18,66 +18,9 @@ const productSchema = new mongoose.Schema({
   isFeatured: { type: Boolean, default: false },
   isOnSale: { type: Boolean, default: false },
   reviews: [reviewSchema],
-<<<<<<< HEAD
-  averageRating: {
-    type: Number,
-    default: 0,
-    min: 0,
-    max: 5
-  },
-  reviewCount: {
-    type: Number,
-    default: 0
-  },
-  soldCount: {
-    type: Number,
-    default: 0
-  },
-  viewCount: {
-    type: Number,
-    default: 0
-  }
-}, {
-  timestamps: true
-});
-
-// Index for search functionality
-productSchema.index({ 
-  title: 'text', 
-  description: 'text', 
-  category: 'text',
-  brand: 'text',
-  material: 'text'
-});
-
-// Calculate average rating when reviews are added/updated
-productSchema.pre('save', function(next) {
-  if (this.reviews.length > 0) {
-    const totalRating = this.reviews.reduce((sum, review) => sum + review.rating, 0);
-    this.averageRating = totalRating / this.reviews.length;
-    this.reviewCount = this.reviews.length;
-  }
-  next();
-});
-
-// Virtual for discounted price
-productSchema.virtual('discountedPrice').get(function() {
-  if (this.isOnSale && this.discountPercentage > 0) {
-    return this.price - (this.price * this.discountPercentage / 100);
-  }
-  return this.price;
-});
-
-// Ensure virtual fields are serialized
-productSchema.set('toJSON', { virtuals: true });
-productSchema.set('toObject', { virtuals: true });
-
-export default mongoose.model('Product', productSchema);
-=======
   rating: { type: Number, default: 0 },
   numReviews: { type: Number, default: 0 },
 }, { timestamps: true });
 
 const Product = mongoose.model("Product", productSchema);
 export default Product;
->>>>>>> origin/bimzed
