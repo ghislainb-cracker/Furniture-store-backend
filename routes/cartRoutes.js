1e5ae5739e379f0b068fb0a7a4675ba15fe5b611
Ghislain Byimbo <byimbog250@gmail.com>
2025-08-09 23:26:09 +0200

    fifteen commit

---
+++ routes/cartRoutes.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { getCart, addToCart, updateCartItem, removeFromCart, clearCart } from "../controllers/cartController.js"
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.get('/', protect, getCart);
+router.post('/add', addToCart);
+router.put('/update/:itemId', updateCartItem);
+router.delete('/remove/:itemId', removeFromCart);
+router.delete('/clear', clearCart);
+
+export default router
